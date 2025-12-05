ae9a45da00c6f0784a8b75b776eb10c1e4a39903
Davit Uturashvili <uturashvili.david@gmail.com>
2025-12-05 16:42:32 +0400

    added AnyPair,FigurePair classes. Changed Main class

--- src/main/java/ge/tbc/testautomation/javaoop/runners/Main.java
+++ src/main/java/ge/tbc/testautomation/javaoop/runners/Main.java
@@ -1,42 +1,34 @@
 package ge.tbc.testautomation.javaoop.runners;
 
 
-import ge.tbc.testautomation.annotationsAndStreams.Analyzable;
-import ge.tbc.testautomation.annotationsAndStreams.VariableNameAnnotation;
+import ge.tbc.testautomation.generics.AnyPair;
+import ge.tbc.testautomation.generics.FigurePair;
+import ge.tbc.testautomation.javaoop.figures.Circle;
+import ge.tbc.testautomation.javaoop.figures.Rectangle;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    @SuppressWarnings("unused")
     public static void main(String[] args) {
+        AnyPair<Field[], Field[]> random = getDeclaredFields(25,"forzaMilan");
+        List<Field> intFields = Arrays.asList(random.getElementOne());
+        List<Field> stringFields = Arrays.asList(random.getElementTwo());
+        System.out.println("Integer Fields:" + intFields + "\n" +"String Fields:" + stringFields);
 
-        List<Field> matchedFields = Arrays.stream(Analyzable.class.getDeclaredFields()).filter(
-                field -> {
-                    VariableNameAnnotation fieldAnnotation = field.getAnnotation(VariableNameAnnotation.class);
-                    return fieldAnnotation != null &&
-                            field.getName().equalsIgnoreCase(fieldAnnotation.name());
-                }
-        ).toList();
+        Circle circle = new Circle(7.7);
+        Rectangle rectangle = new Rectangle(3,6);
 
-        List<Field> unmatchedFields = Arrays.stream(Analyzable.class.getDeclaredFields()).filter(
-                field -> {
-                    VariableNameAnnotation fieldAnnotation = field.getAnnotation(VariableNameAnnotation.class);
-                    return fieldAnnotation == null ||
-                            !field.getName().equalsIgnoreCase(fieldAnnotation.name());
-                }
-        ).toList();
+        FigurePair<Circle,Rectangle> rectangleAndCircle = new FigurePair<>(circle,rectangle);
+        System.out.println(rectangleAndCircle);
+    }
 
-        System.out.println(matchedFields);
-        System.out.println(unmatchedFields);
+    private static <K, D> AnyPair<Field[], Field[]> getDeclaredFields(K objOne, D objTwo) {
+        Field[] firstFields = objOne.getClass().getDeclaredFields();
+        Field[] secondFields = objTwo.getClass().getDeclaredFields();
 
-        //unused fields
-        String neverUsed = "HELLO";
-        int myAge = 25;
-        int[] salaries = {20000,123000,5000};
-
-
+        return new AnyPair<>(firstFields, secondFields);
 
     }
 }
